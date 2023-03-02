8c8c9a52908549f9a95098ece25712b7d592e546
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-03-02 13:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let input = document.getElementById("input");
+let expression = "";
+function solve(e) {
+  // console.log(e)
+  if (e === "ans") {
+    // evaluate the expression
+    let result = eval(expression);
+    console.log(result);
+    input.value = result;
+  } else if (e === "clear") {
+    expression = "";
+    input.value = "";
+  } else {
+    expression += e;
+    console.log(expression);
+    input.value = expression;
+  }
+}
